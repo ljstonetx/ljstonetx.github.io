5b257cad7e8dcdca575240adaf493bf4330ee594
ljstonetx <lisajensenstone@gmail.com>
2025-05-13 06:03:26 -0500

    Add files via upload

--- doit.py
+++ doit.py
@@ -21,277 +21,202 @@
 
 
 import csv
-conDescription= "<div><p1>Discover the rich history of Mercedes, Texas, through the website mercedestx.com. This site offers a captivating glimpse into the city's early 20th-century transformation from ranching to commercial agriculture, showcasing its dynamic growth during that period. Explore a curated collection of historic photographs and narratives that highlight significant landmarks, such as architecturally notable schools along Ohio Street, the grand Mercedes Hotel built in 1907, and pivotal military training camps like Camp Mercedes and Camp Llano Grande. The website also delves into the development of the city's irrigation system, early industries, and the evolution of its downtown area. Whether you're a history enthusiast or a curious traveler, here you find an engaging journey through the heritage of this charming South Texas town. Source images are from digital collections at Texas State Library, University of Texas and SMU.  We welcome feedback and comments at lisajensenstone@gmail.com.</div></p1>"
-conSiteShortTitle = "Mercedes Historic Photograps"
-conSiteLongTitle = "Mercedes Texas 1900s to 1950s History and Images"
-conEmailAddress = "mercedestx@gmail.com"
-conNumCards=200
-conNumHxRows=47
-conNumHxTopics=4
-conNumSubjects=23  #includes Citations in a kludge
-conPostCardFile='PostcardSorted.csv'
-#conPostCardFile='newcards.csv'
-conSubjectsFile='postcardViewsSorted.csv'
-conHistoryFile='History.csv'
-conFileSubjects='index.html'
-conHistoryTextFile='historyText.csv'
-conColorFileDir = 'imagesColorized/'
-conCitationsTitle = "Citations for Mercedes History Information" 
-conCitationsFile = "Citations.pdf" 
-conSMULink         = 'https://digitalcollections.smu.edu/digital/collection/tex/id/'
-conUTRGVStudioLink = 'https://scholarworks.utrgv.edu/rgvstudio/' 
-conUTRGVMiscLink   = 'https://scholarworks.utrgv.edu/miscphotosedinburg/'
-conUTRGVMiscLinkBrown = 'https://scholarworks.utrgv.edu/miscphotosbrownsville/'
-conUTRGVMiscLinkHidal = 'https://scholarworks.utrgv.edu/hidalgohist_aa/'  
-conTXHISARCLink = 'https://tsl.access.preservica.com/uncategorized/'
-conSMU = "SMU"
-conUTRGVSTUDIO = "UTRGVSTUDIO"
-conUTRGVMISC   = "UTRGVMISC"
-conUTRGVMISCBROWN   = "UTRGVMISCBROWN"
-conUTRGVHIDAL   = "UTRGVHIDAL"
-conTXHISARC = "TXHISARC"
+from itertools import islice
+conDescription= "<div><p1>Discover the rich history of Mercedes, Texas, through the website mercedestx.com. This site offers a captivating glimpse into the city's early 20th-century transformation from ranching to commercial agriculture, showcasing its dynamic growth during that period. Explore a curated collection of historic photographs and narratives that highlight significant landmarks, such as architecturally notable schools along Ohio Street, the grand Mercedes Hotel built in 1907, and pivotal military training camps like Camp Mercedes and Camp Llano Grande. The website also delves into the development of the city's irrigation system, early industries, and the evolution of its downtown area. Whether you're a history enthusiast or a curious traveler, here you find an engaging journey through the heritage of this charming South Texas town. Source images are from digital collections at Texas State Library, University of Texas and SMU.  We welcome feedback and comments at mercedesimages@gmail.com.</div></p1>"
+# --- Constants ---
+
+SITE_SHORT_TITLE = "Mercedes Historic Photographs"
+SITE_LONG_TITLE = "Mercedes Texas 1900s to 1950s History and Images"
+EMAIL_ADDRESS = "mercedestx@gmail.com"
+
+
+POSTCARD_FILE = 'PostcardSorted.csv'
+SUBJECTS_FILE = 'postcardViewsSorted.csv'
+HISTORY_FILE = 'History.csv'
+HISTORY_TEXT_FILE = 'historyText.csv'
+SUBJECTS_HTML = 'index.html'
+COLOR_IMAGE_DIR = 'imagesColorized/'
+CITATIONS_FILE = 'Citations.pdf'
+CITATIONS_TITLE = "Citations for Mercedes History Information"
+SOURCE_LINKS = {
+    "SMU": 'https://digitalcollections.smu.edu/digital/collection/tex/id/',
+    "UTRGVSTUDIO": 'https://scholarworks.utrgv.edu/rgvstudio/',
+    "UTRGVMISC": 'https://scholarworks.utrgv.edu/miscphotosedinburg/',
+    "UTRGVMISCBROWN": 'https://scholarworks.utrgv.edu/miscphotosbrownsville/',
+    "UTRGVHIDAL": 'https://scholarworks.utrgv.edu/hidalgohist_aa/',
+    "TXHISARC": 'https://tsl.access.preservica.com/uncategorized/'
+}
 conOTHER       = "OTHER"
 conNONE       = "NONE"
 
-def makeHtmlSubjectFilename(subject):  
+#colors
+#COLOR_TITLE = "#cc0000"
+#COLOR_DATE =  "#414141"
+
+COLOR_TITLE = "#b22222"   # Firebrick – a deeper red, still bold but less harsh
+COLOR_DATE = "#555555"    # Medium-dark gray – softer and more legible than #414141
+
+# --- Utility Functions ---
+def make_html_filename(subject):
+    return CITATIONS_FILE if subject == "Citations" else f"PChtml{subject.replace(' ', '')}.html"
+
+def make_image_path(key):
+    return f"{COLOR_IMAGE_DIR}{key}.jpg"
+
+def make_history_image_path(key):
+    return f"{COLOR_IMAGE_DIR}{key}"
     
-    if (subject == "Citations"): 
-        return conCitationsFile
-    catNoSpace = subject.replace(" ", "")
-    return "PChtml" + catNoSpace + ".html"
+def write_image(fw, image_path):
+    #fw.write(f'<div class="flex-wrap"><img src="{image_path}"></div>\n')
+    fw.write('<div class="flex-wrap"><img src="'+ image_path+ '">')
 
-def writeStyle(FW):
-    FW.write('<!DOCTYPE html>')
-    FW.write('<html lang="en">')
-    FW.write('<head>')
-    FW.write('<meta charset="UTF-8">')
-    FW.write('<meta name="viewport" content="width=device-width, initial-scale=1.0">')
-    FW.write('<title>' + conSiteShortTitle + '</title>')
-    FW.write('<link rel="stylesheet" href="flexCss.css">')
-    FW.write('</head><body><div>')
+def write_image_enlarged(fw, image_path):
+    fw.write(f'<a href="{image_path}" class="button">View Enlarged</a>&nbsp;&nbsp;\n')
 
-def writeShortHeading(FW, heading):
+def write_style(fw):
+    fw.write(f'''<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>{SITE_SHORT_TITLE}</title>
+<link rel="stylesheet" href="flexCss.css">
+</head>
+<body><div>
+''')
 
-    FW.write('<p2>' + heading +'<br></p2></div>')    
+
+def write_heading(fw, heading, is_home=False):
+    fw.write('<div id="flexHeader">\n')
+    if is_home:
+        fw.write(f'<p2>{heading}<br></p2>\n')
+    else:
+        fw.write(f'<p2>{heading}<br><br><a href="{SUBJECTS_HTML}" class="button" target="_blank">Home</a></p2>\n')
+    fw.write('</div><br>\n')
+
      
-
-def writeLongHeading(FW, heading):
-
-    FW.write('<p2>' + heading + '<br><br><a href=' + conFileSubjects + ' class="button" target="_blank">Home</a>'+ '</p2></div>')
-     
-def writeTitle(FW, title):
+def write_history_title(FW, title):
     FW.write('<p1><div>')
-    writeTitle2(FW, title)
+    FW.write('<font color="+COLOR_TITLE"+"> <strong>' + title + ' </strong></font>')
     
-def writeTitle2(FW, title):
-    FW.write('<font color="#cc0000"> <strong>' + title + ' </strong></font>')
+def write_title(FW, title):
+    FW.write('<font color="+COLOR_TITLE"+"> <strong>' + title + ' </strong></font>')
 
-def makeColorFileName(key):
-    return conColorFileDir + key + ".jpg" 
-
-def makeHistoryFileName(key):
-    return conColorFileDir + key     
-
-def writeHomeHeader(FW):
-
+def write_header(FW, subject, heading):
     FW.write('<div id="flexHeader">')
-    writeShortHeading(FW,conSiteLongTitle)
-    FW.write('<br>')  
-    
-    FW.write(conDescription)
-    FW.write('<br>')      
-
-def writeHeader(FW, subject, heading):
-    FW.write('<div id="flexHeader">')
-    writeLongHeading(FW, heading)
+    FW.write('<p2>' + heading + '<br><br><a href=' + SUBJECTS_HTML + ' class="button" target="_blank">Home</a>'+ '</p2></div>')
     FW.write('<div id="flexHeader">')
     FW.write('</div><br>')
 
-def writeCitations(FW):
-    FW.write('<div>View source citations:  <a href=' + conCitationsFile + 'class="button">' + 'View Citations </a></div>')
+def write_citations(FW):
+    FW.write('<div>View source citations:  <a href=' + CITATIONS_FILE + 'class="button">' + 'View Citations </a></div>')
 
-def writeDate(FW, date):
-    FW.write('<font color="#414141">' + date + ' </font>')                       
+def write_date(fw, date):
+    fw.write('<font color='+COLOR_DATE+'>'+date+'</font>\n')
 
-def writeDescription(FW, description):
-    FW.write('<br><br><p1>' +   description  + '</p1>')
-    FW.write('<br><br>')
-
-def writeImage(FW, imageFile): 
-    FW.write('<div class="flex-wrap"><img src="'+ imageFile+ '">')
-
-
-def writeSourceLink(FW, imageSource, imageId):
-    conLibButton = '/ class="button">View Library</a>&nbsp;&nbsp;'
-    if imageSource == conSMU:
-        FW.write('<a href=' + conSMULink +  imageId          + conLibButton)
-    elif imageSource == conUTRGVSTUDIO: 
-        FW.write('<a href=' + conUTRGVStudioLink + imageId   + conLibButton)
-    elif imageSource == conUTRGVMISC:
-        FW.write('<a href=' + conUTRGVMiscLink + imageId     + conLibButton)
-    elif imageSource == conUTRGVMISCBROWN:
-        FW.write('<a href=' + conUTRGVMiscLinkBrown + imageId     + conLibButton)
-    elif imageSource == conOTHER:
-        FW.write('<a href=' + imageId                        + '/ class="button">View Source Article</a>')
-    elif imageSource == conUTRGVHIDAL:
-        FW.write('<a href=' + conUTRGVMiscLinkHidal + imageId  + conLibButton)
-    elif imageSource == conTXHISARC:
-        FW.write('<a href=' + conTXHISARCLink  + imageId + conLibButton)
-    #elif imageSource == conNONE: do nothing
-           
-def writeImageEnlarged(FW, imageFile):    
-    FW.write('<a href=' + imageFile + ' class="button">' + 'View Enlarged</a>&nbsp;&nbsp;')
-
-
-def writeSubjects():
-
-    subjectIdx =0
-    keyIdx = 2
-    descriptionIdx =3
-    headingIdx=4
-    FW= open(conFileSubjects, "w+") 
+def write_description(fw, description):
+    fw.write(f'<br><br><p1>{description}</p1><br><br>\n')
     
-    writeStyle(FW)   
+def write_source_link(fw, source, source_id):
+    if source == "NONE": return
+    if source in SOURCE_LINKS:
+        fw.write(f'<a href="{SOURCE_LINKS[source]}{source_id}" class="button">View Library</a>&nbsp;&nbsp;\n')
+    else:
+        fw.write(f'<a href="{source_id}" class="button">View Source Article</a>\n')
 
-    writeHomeHeader(FW) 
+def write_home_intro(fw):
+    write_heading(fw, SITE_LONG_TITLE, is_home=True)
+    intro = (
+        "<div><p1>This website offers a glimpse into the early history of Mercedes, Texas. "
+        "In the early 1900s, Mercedes and the Lower Rio Grande Valley underwent a transformative shift, "
+        "moving from traditional ranching to commercial agriculture. This set the stage for significant growth "
+        "and marked a dynamic period in the regional development. Explore the historical context on the Citations page. "
+        "Press the View Library button for image citations.Send feedback to mercedesimages@gmail.com.</p1></div><br>"
+    )
+    fw.write(intro)
 
-    from itertools import islice
-    with open(conSubjectsFile) as csvfile:
-        reader1 = csv.reader(csvfile)       
-        for line in islice(reader1, conNumSubjects): 
-           
-           subject = line[subjectIdx]
-           key = line[keyIdx] 
-           description = line[descriptionIdx]
-           heading = line[headingIdx]
-           imageFile = makeColorFileName(line[keyIdx])
-           if subject != "subject": #skip first line
+def write_subjects():
+    with open(SUBJECTS_HTML, "w", encoding="utf-8") as fw:
+        write_style(fw)
+        write_home_intro(fw)
 
-               writeImage(FW, imageFile)
-               FW.write('<p1>')
-               writeTitle2(FW, heading) 
-               writeDescription(FW, description)
-               htmlName= makeHtmlSubjectFilename(subject);
-               FW.write('<a href=' + htmlName         + ' class="button">' + 'View '+ subject + '</a>')
-               FW.write('</div><br>')
+        with open(SUBJECTS_FILE, newline='') as csvfile:
+            reader = csv.reader(csvfile)     
+            for line in islice(reader,1, None):
+               subject, _, key, description, heading = line[:5]
                
-               if (subject == "Fuste" or subject == "Colegio" or subject == "Rio Rico"): 
-                writeHxSubjectFile(subject, heading)
+               image_path = make_image_path(key)
+               write_image(fw, image_path)
+               fw.write('<p1>')
+               write_title(fw, heading) 
+               write_description(fw, description)
+               htmlName= make_html_filename(subject);
+               fw.write('<a href=' + htmlName         + ' class="button">' + 'View '+ subject + '</a>')
+               fw.write('</div><br>')
+               
+               if subject in {"Fuste", "Colegio", "Rio Rico"}:
+                    write_history_subject(subject,heading)
                elif (subject != "Citations"):
                     # Citations.pdf file was made by importing citations.csv into a google doc, format order as
                     # number, wrapping the citation column, and exporting to pdf. Then copying file to source code directory               
-                   writeSubjectFile(subject, heading) 
-                       
-           
-        writeHeader(FW, conSiteLongTitle, conSiteLongTitle)
-        FW.write('</body></html>')
-        
-def writeSubjectFile(subject, heading): 
- 
-    filename = makeHtmlSubjectFilename(subject);
-    #then editing it in Open Office Calc to sort on the score column
-    
-    keyIdx = 0
-    sourceIdx = 1
-    idIdx=2    
-    scoreIdx =3
-    headingIdx =4
-    subjectIdx =5
-    dateIdx =6
-    descriptionIdx =7
-    FW= open(filename, "w+")    
-    writeStyle(FW)   
-    writeHeader(FW, subject, heading ) 
+                   
+                   write_postcard_subject(subject, heading)
 
-    from itertools import islice
-    with open(conPostCardFile) as csvfile:
-        reader = csv.reader(csvfile)       
-        for row in islice(reader, conNumCards):         
-            if row[subjectIdx] != subject: continue
+        write_citations(fw)
+        write_heading(fw, SITE_LONG_TITLE, heading)
+        fw.write('</body></html>')
+
+def write_postcard_subject(subject, heading):
+    filename = make_html_filename(subject)
+    with open(filename, "w", encoding="utf-8") as fw:
+        write_style(fw)
+        write_heading(fw, heading)
+  
+        with open(POSTCARD_FILE, newline='') as csvfile:
+            reader = csv.reader(csvfile)
             
-            if row[scoreIdx] == "0": continue
-            imageFile = makeColorFileName(row[keyIdx])
-            writeImage(FW,imageFile)
-            FW.write('<p1>')                     
-            writeDate(FW, row[keyIdx])                    
-            writeTitle2(FW, row[headingIdx])
-            writeDate(FW, row[dateIdx])
-            writeDescription(FW, row[descriptionIdx])
-            writeImageEnlarged(FW,imageFile)
-            writeSourceLink(FW, row[sourceIdx], row[idIdx])
-            FW.write('<br><br>') 
-            FW.write('</div>')
+            for row in islice(reader,1,None):   #start at row 1, None = go to end
+                if row[5] != subject or row[3] == "0": continue
+
+                key, source, source_id, _, heading_text, _, date, description = row[:8]
+                image_path = make_image_path(key)
+                write_image(fw, image_path)
+                fw.write('<p1>') #added
+                write_date(fw, key)
+                write_title(fw, heading_text)
+                write_date(fw, date)
+                write_description(fw, description)
+                write_image_enlarged(fw, image_path)
+                write_source_link(fw, source, source_id)               
+                fw.write('<br><br>') #added
+                fw.write('</div>')   #added
                 
-        writeHeader(FW,subject, heading) 
-        FW.write('</body></html>')
-        
-def writeHxSubjectFile(subject, heading):
- #"Title","topic","imagePic","citationText","citationDate","summary","citationLink"
-    filename = makeHtmlSubjectFilename(subject);  
-    titleIdx = 0
-    
-    subjectIdx = 1
-    imagePicIdx=2    
-    citationTextIdx =imagePicIdx+1
-    dateIdx = citationTextIdx+1
-    summaryIdx = dateIdx+1
-    citationLinkIdx = summaryIdx+1
-    FW= open(filename, "w+")    
-    writeStyle(FW)   
-    writeHeader(FW,subject, heading ) 
 
-    from itertools import islice
-    with open(conHistoryFile) as csvfile:
-        reader = csv.reader(csvfile)       
-        for row in islice(reader,conNumHxRows): 
-            if row[subjectIdx] != subject: continue
-            title = row[titleIdx]
-            date = row[dateIdx]
-            imageFile = makeHistoryFileName(row[imagePicIdx])   
-            citationText = row[citationTextIdx]
-            citationDate = row[dateIdx]
-            summary = row[summaryIdx]
-            citationLink = row[citationLinkIdx]
-            writeImage(FW,imageFile)  
-            writeTitle(FW, title)
-            writeDate(FW, date)            
-            writeDescription(FW, summary) 
-            writeImageEnlarged(FW,imageFile)
-            writeSourceLink(FW, conOTHER, citationLink)
-            FW.write('</div></div><br><br>')
-        writeHeader(FW,subject, heading ) 
-        FW.write('</body></html>')  
+        write_heading(fw, heading)
+        fw.write('</body></html>')
             
-def writeHxSubjectTextFile(subject):
-    #"Title","topic","imagePic","citationText","citationDate","summary","citationLink"
-    filename = makeHtmlSubjectFilename(subject);
-    #This file was created by exporting the postcards table from wix, then editing to remove single quotes, 
-    #then editing it in Open Office Calc to sort on the score column
-    #"heading","topic","description","image","imageHeader","score"
-    titleIdx = 0   
-    subjectIdx = 1
-    summaryIdx = 2
-    imagePicIdx= 3
-    scoreIdx= 4
- 
-    FW= open(filename, "w+")    
-    writeStyle(FW)   
-    FW.write('</head><body><div>')
-    writeHeader(FW, subject) 
+def write_history_subject(subject, heading):
+    filename = make_html_filename(subject)
+    with open(filename, "w", encoding="utf-8") as fw:
+        write_style(fw)
+        write_heading(fw, heading)
 
-    from itertools import islice
-    with open(conHistoryTextFile) as csvfile:
-        reader = csv.reader(csvfile)       
-        for row in islice(reader,conNumHxTopics): 
-            if row[subjectIdx] != subject: continue         
-            writeImage(FW,makeColorFileName(row[imagePicIdx]))
-            writeTitle(FW, row[titleIdx])
-            writeDescription(FW, row[summaryIdx])
-            FW.write('</div></div>')
-            writeHxSubjectFile(FW, subject)
-        writeHeader(FW,subject) 
-        FW.write('</body></html>')
+        with open(HISTORY_FILE, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in islice(reader,1,None):
+                title, _, image, citation_text, date, summary, link = row[:7]
+                image_path = make_history_image_path(image)
+                write_image(fw, image_path)
+                write_history_title(fw,title)
+                write_date(fw, date)
+                write_description(fw, summary)
+                write_image_enlarged(fw, image_path)
+                write_source_link(fw, "OTHER", link)
+                fw.write('</div></div><br><br>')
 
-writeSubjects()
+        write_heading(fw, heading)
+        fw.write('</body></html>')
+
+write_subjects()
 
 #check this out https://css-tricks.com/snippets/css/a-guide-to-flexbox/#aa-flexbox-tricks
